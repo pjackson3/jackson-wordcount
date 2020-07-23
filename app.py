6043407dfc0c0dae74ac1e73207625ce7c40b8b6
Peter Jackson Link III <pjacksonlink@gmail.com>
2020-07-23 11:02:15 -0400

    Change

--- app.py
+++ app.py
@@ -13,5 +13,11 @@ def hello():
     return "Hello, World!"
 
 
+@app.route('/<name>')
+def hello_name(name):
+    """Say hello to someone's name."""
+    return "Hello, {}".format(name)
+
+
 if __name__ == "__main__":
     app.run()
